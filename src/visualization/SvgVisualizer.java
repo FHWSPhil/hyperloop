dbc68581a018f50796f4b29564b80d770c5eaaa9
FHWSPhil <philipp.tscherner@student.fhws.de>
2021-01-05 15:39:51 +0100

    visualisation is final

--- src/visualization/SvgVisualizer.java
+++ src/visualization/SvgVisualizer.java
@@ -17,27 +17,52 @@ public class SvgVisualizer {
 			
 		bw.write("<html> \n <body> \n");
 		
-//		bw.write("<svg width=\"10000\" height=\"10000\" viewBox= \"0 0 10000 10000\" style=\"transform: scale(1,-1)\">\n");
-		bw.write("<svg width=\"10000\" height=\"10000\" viewBox= \"0 0 10000 10000\">\n");
+		bw.write("<svg width=\"5000\" height=\"5000\" xmlns=\"http://www.w3.org/2000/svg\" viewBox= \"0 0 10000 10000\" style=\"transform: scale(1,-1)\">\n");
 		
-		//Schreibe Punkte
+		//Triangle as marker of Coordinate-Axis-End
+		bw.write("<marker id=\"triangle\" viewBox=\"0 0 10 10\" refX=\"0\" refY=\"5\" markerUnits=\"strokeWidth\" markerWidth=\"4\" markerHeight=\"3\" orient=\"auto\">\r\n"
+				+ " <path d=\"M 0 0 L 10 5 L 0 10 z\"/>\r\n"
+				+ " </marker>");
+		
+		//xAxis
+		bw.write("<line x1=\"0\" y1=\"0\" x2=\"9940\" y2=\"0\" marker-end=\"url(#triangle)\" stroke=\"black\" stroke-width=\"20\"/>\n");
+		//Description of x-Axis
+		bw.write("<circle cx=\"10000\" cy=\"0\" r=\"40\" stroke=\"black\" stroke-width=\"20\" fill=\"none\" />\n");
+		bw.write("<text x=\"9300\" y=\"-20\" font-size=\"100\" transform=\"scale(1, -1)\" > Lon: " + 53 + " Lat: " + 13 + "</text>\n");
+		
+		//yAxis
+		bw.write("<line x1=\"0\" y1=\"0\" x2=\"0\" y2=\"9940\" marker-end=\"url(#triangle)\" stroke=\"black\" stroke-width=\"20\"/>\n");
+		//Description of y-Axis
+		bw.write("<circle cx=\"0\" cy=\"10000\" r=\"40\" stroke=\"black\" stroke-width=\"20\" fill=\"none\" />\n");
+		bw.write("<text x=\"40\" y=\"-9900\" font-size=\"100\" transform=\"scale(1, -1)\" > Lon: " + 52 + " Lat: " + 14 + "</text>\n");
+		
+		//Description of 0-Point
+		bw.write("<circle cx=\"0\" cy=\"0\" r=\"40\" stroke=\"black\" stroke-width=\"20\" fill=\"none\" />\n");
+		bw.write("<text x=\"40\" y=\"-20\" font-size=\"100\" transform=\"scale(1, -1)\" > Lon: " + 52 + " Lat: " + 13 + "</text>\n");
+		
+		//ledger lines for coordinate system
+		for(int i = 100; i <= 10000; i+=100) {
+			bw.write("<line x1=\"" + i + "\" y1=\"0\" x2=\"" + i + "\" y2=\"10000\" stroke=\"black\" stroke-width=\"1\" stroke-dasharray=\"5\"/>\n");
+			bw.write("<line x1=\"0\" y1=\"" + i + "\" x2=\"10000\" y2=\"" + i + "\" stroke=\"black\" stroke-width=\"1\" stroke-dasharray=\"5\"/>\n");
+		}
+		
+		
+
+		//Stations
 		for(Coordinate coordinate : coordinates) {
-			int x = (int) ((coordinate.getX()-52)*10000);//-3300;
-			int y = (int) ((coordinate.getY()-13)*10000);//-800;
-//			System.out.println("x: " + x);
-//			System.out.println("y: " + y);
+			int x = (int) ((coordinate.getX()-52)*10000);
+			int y = (int) ((coordinate.getY()-13)*10000);
 			
 			bw.write("<g id=\"" + coordinate.getId() + "\">\n");
 			bw.write("<circle cx=\"" + x + "\" cy=\"" + y + "\" r=\"10\" stroke=\"black\" stroke-width=\"1\" fill=\"yellow\" /> \n");
 			
-//			bw.write("<g transform=\"translate(0, 10000) scale(1, -1) rotate(-90)\">\n");
-			bw.write("<text x=\"" + x + "\" y=\"" + (y+2) + "\" "
+			bw.write("<text x=\"" + x + "\" y=\"" + -(y-10) + "\" transform=\"scale(1, -1)\" "
 					+ "font-size=\"6\" text-anchor=\"middle\">" + coordinate.getStationName() + "</text>\n");
-//			bw.write("</g>\n");
 			bw.write("</g>\n");
 		}
 		
-		//Schreibe Linie
+		
+		//Hyperloop-Path
 		bw.write("<line x1=\"" + start.getX() + "\" y1=\"" + start.getY() //
 				+ "\" x2=\"" + end.getX() + "\" y2=\"" + end.getY() //
 				+ "\" style=\"stroke:rgb(255,0,0);stroke-width:2\" /> \n");
